f326776a520de86fd6601117d6f4cc22f90c1ef2
flavianogjc <flavianogjc@hotmail.com>
2019-07-02 23:02:37 -0300

    Colorindo Tetraedros

---
+++ matematica/2890/2890.py
@@ -0,0 +1,11 @@
+# https://oeis.org/A006008
+
+if __name__ == '__main__':
+    while (True):
+        n = input()
+        if (n == 0):
+            break
+        n2 = n * n
+        n4 = n2 * n2
+
+        print ((11 * n2 + n4) / 12) % 1000007
