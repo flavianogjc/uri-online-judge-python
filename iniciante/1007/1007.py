efb24c1ce4676dc32a64343ea2b56e317b2deef0
flavianogjc <flavianogjc@hotmail.com>
2019-07-03 23:43:08 -0300

    Diferença

---
+++ iniciante/1007/1007.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    A, B, C, D = float(raw_input()), float(raw_input()), float(raw_input()), float(raw_input())
+
+    deferenca = A * B - C * D
+
+    print("DIFERENCA = %d" % deferenca)
