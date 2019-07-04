ceb2b5888219fa6cd64148b571b87dfa56134bb6
flavianogjc <flavianogjc@hotmail.com>
2019-07-03 23:59:22 -0300

    Salário

---
+++ iniciante/1008/1008.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = input()
+    horas = input()
+    valorHora = float(raw_input())
+
+    print("NUMBER = %d" % n)
+    print("SALARY = U$ %.2f" % (horas * valorHora))
