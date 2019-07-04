a0e0e43e12f847342b0b482af7ed1e24ef4e9950
flavianogjc <flavianogjc@hotmail.com>
2019-07-03 23:43:33 -0300

    Média 2

---
+++ iniciante/1006/1006.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    A, B, C = float(raw_input()), float(raw_input()), float(raw_input())
+
+    media = (2.0 * A + 3.0 * B + 5.0 * C) / 10.0
+
+    print("MEDIA = %.1f" % media)
