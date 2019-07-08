31e0ec67f4362f174597bdb9f5ac535fe602e51c
flavianogjc <flavianogjc@hotmail.com>
2019-07-07 22:00:19 -0300

    Soma de Fatoriais

--- matematica/1161/1161.py
+++ matematica/1161/1161.py
@@ -1,7 +1,7 @@
 from math import factorial as fat
 
 if __name__ == '__main__':
-    while (True):
+    while True:
         try:
             n, m = map(int, raw_input().split())
             print(fat(n) + fat(m))
