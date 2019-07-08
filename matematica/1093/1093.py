148e7bf6e88bb0d8727602d7da3a56d846aa998b
flavianogjc <flavianogjc@hotmail.com>
2019-07-07 21:59:21 -0300

    Vampiros

--- matematica/1093/1093.py
+++ matematica/1093/1093.py
@@ -1,15 +1,15 @@
 # Gambler's ruin
 
 if __name__ == '__main__':
-    while (True):
+    while True:
         EV1, EV2, AT, D = map(int, raw_input().split())
-        if (EV1 == 0 and EV2 == 0 and AT == 0 and D == 0):
+        if EV1 == 0 and EV2 == 0 and AT == 0 and D == 0:
             break
 
         n1 = EV1 / D + (1 if (EV1 % D) else 0)
         n2 = EV2 / D + (1 if (EV2 % D) else 0)
 
-        if (AT == 3):
+        if AT == 3:
             print("%.1lf" % (n1 * 100.0 / (n1 + n2)))
         else:
             p = AT / 6.0
