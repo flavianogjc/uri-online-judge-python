fdd4f5fcb10e9f8884861576c9a4482cca7f89bc
flavianogjc <flavianogjc@hotmail.com>
2019-07-10 19:53:27 -0300

    CPF 2

---
+++ ad-hoc/1786/1786.py
@@ -0,0 +1,23 @@
+def validator(num):
+    v1, v2, i = 0, 0, 1
+    cpf = ""
+    for d in map(int, num):
+        v1 += d * i
+        v2 += d * (10 - i)
+        cpf += str(d) + ("." if (i % 3 == 0 and i != 9) else "")
+        i += 1
+
+    v1 = (v1 % 11) % 10
+    v2 = (v2 % 11) % 10
+
+    return cpf + "-" + str(v1) + str(v2)
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            n = raw_input()
+        except:
+            break
+
+        print(validator(n))
