f88b6be635e64f2bdbafe56e7b648d366fb21c1b
flavianogjc <flavianogjc@hotmail.com>
2019-07-13 21:21:04 -0300

    Balão++

--- geometria/2840/2840.py
+++ geometria/2840/2840.py
@@ -3,7 +3,7 @@ from math import floor
 if __name__ == '__main__':
     r, l = map(float, raw_input().split())
 
-    v_esfera = (4.188666) * r * r * r # Volume da esfera
-    v_botijao = l   # Volume do botijão..
+    v_esfera = (4.188666) * r * r * r  # Volume da esfera
+    v_botijao = l  # Volume do botijão..
 
     print(int(floor(v_botijao / v_esfera)))
